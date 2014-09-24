5a24d6b6de76f0a7048f4f585f843d66fdae2370
jcjcarter <Ace@Jaysons-MacBook-Pro.local>
2014-09-24 00:13:01 -0500

    Started a new branch for 412
    Trying a different and correction implementation.

---
+++ HW04/src/paint/ADecoratorPaintStrategy.java
@@ -0,0 +1,28 @@
+package paint;
+
+import java.awt.Graphics;
+import java.awt.geom.AffineTransform;
+
+import balls.Ball;
+
+public class ADecoratorPaintStrategy extends APaintStrategy {
+
+  public ADecoratorPaintStrategy(AffineTransform a) {
+    super(a);
+    // TODO Auto-generated constructor stub
+  }
+
+  /*public ADecoratorPaintStrategy( APaintStrategy decoree) {
+   
+  }*/
+
+  public void init(Ball host){}
+  
+  public void paint(Graphics g, Ball host){}
+  
+  @Override
+  public void paintXfrm(Graphics g, Ball host, AffineTransform af) {
+    
+  }
+
+}
